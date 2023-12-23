022b7ae90618d0419fc89d57723266487d2e6d15
Shiva <shivabhattarai15@gmail.com>
2023-12-23 15:54:42 +0545

    Add routes to existing pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./Pages/Root";
+import Error from "./Pages/Error";
+import Home from "./Pages/Home";
+import NewLabel from "./Pages/NewLabel";
+import Label from "./Pages/Label";
+
 function App() {
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <Error />,
+      children: [
+        {
+          index: true,
+          element: <Home />
+        },
+        {
+          path: "/new",
+          element: <NewLabel />
+        },
+        {
+          path: "/label/:labelId",
+          element: <Label />
+        }
+      ]
+    }
+  ])
   return (
-    <h1>App</h1>
+    <RouterProvider router={routes} />
   );
 }
 
