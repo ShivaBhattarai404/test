9b990ba29d8bc0f711a4cfb304471880e7d29549
Shiva <shivabhattarai15@gmail.com>
2023-12-25 03:23:48 +0545

    Add few button variants and updated its stylesheet

--- src/Components/UI/Button/Button.jsx
+++ src/Components/UI/Button/Button.jsx
@@ -2,21 +2,26 @@ import React from "react";
 import classes from "./Button.module.css";
 
 const Button = (props) => {
-    const {variant} = props;
-    let classnames = "";
-   switch(variant){
+  const { variant } = props;
+  let classnames = "";
+  switch (variant) {
     case "success":
-        classnames = classes.success;
-        break;
+      classnames = classes.success;
+      break;
     case "danger":
-        classnames = classnames.danger;
-        break;
+      classnames = classes.danger;
+      break;
+    case "peace":
+      classnames = classes.peace;
+      break;
     default:
-        classnames = "";
-   }
+      classnames = "";
+  }
   return (
     <button
-      className={`${classes.btn} ${props.className ? props.className : ""} ${classnames}`}
+      className={`${classes.btn} ${props.className ? props.className : ""} ${
+        props.animated =="true" && classes.animated
+      } ${classnames}`}
       style={{ ...props.style }}
       onClick={props.onClick}
     >
