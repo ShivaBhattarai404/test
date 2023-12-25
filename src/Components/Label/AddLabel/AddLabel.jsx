63bdd7c4b5c43c22b1938de6cbe2e4c3076d4904
Shiva <shivabhattarai15@gmail.com>
2023-12-25 10:17:36 +0545

    Create AddLabel component to add new Labels

---
+++ src/Components/Label/AddLabel/AddLabel.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import classes from "./AddLabel.module.css";
+import { Form } from "react-router-dom";
+import Input from "../../UI/Input/Input";
+import Button from "../../UI/Button/Button";
+
+const AddLabel = () => {
+  return (
+    <section className={classes.section}>
+      <h1 className={classes.title}>Add New Label</h1>
+      <Form className={classes.form} method="POST" action="/new">
+        <Input
+          className={classes.formLabelName}
+          name="labelName"
+          title="Label Name"
+          required="true"
+        />
+        <Input
+          title="Budget"
+          required="true"
+          name="labelBudget"
+          type="number"
+          step="0.1"
+        />
+        <Button variant="success" type="submit" className={classes.saveBtn}>
+          Save
+        </Button>
+      </Form>
+    </section>
+  );
+};
+
+export default AddLabel;
