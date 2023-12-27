d324f0f505e40bbffffa2567aa469194c6ecb099
Shiva <shivabhattarai15@gmail.com>
2023-12-27 20:50:58 +0545

    Add authentication checker middleware

---
+++ backend/middleware/is_auth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get("Authorization");
+  if (!authHeader) {
+    const error = new Error("Not Authenticated");
+    error.status = 401;
+    return next(error);
+  }
+  const token = authHeader.split(" ")[1];
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, "kerwani123");
+  } catch (error) {
+    return next(error);
+  }
+  if (!decodedToken) {
+    const error = new Error("Not Authenticated");
+    error.status = 401;
+    return next(error);
+  }
+  req.userId = decodedToken.userId;
+  next();
+};
