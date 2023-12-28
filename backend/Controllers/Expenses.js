d28067280c13ca73e916175fd1db68247715961a
Shiva <shivabhattarai15@gmail.com>
2023-12-28 20:31:22 +0545

    Add Labels and hasLabelId to expenses routes

--- backend/Controllers/Expenses.js
+++ backend/Controllers/Expenses.js
@@ -1,11 +1,14 @@
 const Expenses = require("../Model/Expenses");
-const User = require("../Model/User");
+const Label = require("../Model/Labels");
 
 exports.getExpenses = async (req, res, next) => {
+  const labelId = req.params.labelId;
   try {
-    const user = await User.findById(req.userId).populate('expenses');
-    res.status(200).json({ expenses: user.expenses });
+    const label = await Label.findById(labelId).populate("expenses");
+    res.status(200).json({ expenses: label.expenses });
   } catch (error) {
+    error.message = "Invalid label Id";
+    error.status = 400;
     return next(error);
   }
 };
@@ -14,31 +17,41 @@ exports.addExpense = async (req, res, next) => {
   const name = req.body.name;
   const amount = req.body.amount;
 
-  const newExpense = new Expenses({
+  const expense = new Expenses({
     name: name,
     amount: amount,
+    label: req.label._id,
   });
   try {
-    const expense = await newExpense.save();
-    const user = await User.findById(req.userId);
-    const updatedExpenses = [...user.expenses, expense._id];
-    user.expenses = updatedExpenses;
-    await user.save();
-    res.status(201).json({ message: "Expense added", expense: expense });
+    const uploadedExpense = await expense.save();
+    req.label.expenses.push(uploadedExpense);
+    await req.label.save();
+    res
+      .status(201)
+      .json({ message: "Expense added", expense: uploadedExpense });
   } catch (error) {
     return next(error);
   }
 };
 
-exports.deleteExpense = async (req, res, next) =>{
-    const expenseId = req.body.id;
-    const user = await User.findById(req.userId);
+exports.deleteExpense = async (req, res, next) => {
+  const expenseId = req.body.id;
 
-    const updatedExpenses = user.expenses.filter(expense => expense._id.toString() !== expenseId.toString());
-    user.expenses = updatedExpenses;
-    await user.save();
-    
-    const expense = await Expenses.findByIdAndDelete(expenseId);
-    const updatedUser = await User.findById(req.userId).populate('expenses');
-    res.status(200).json({message: "expense deleted successfully", expenses: updatedUser.expenses});
-}
+  try {
+    // remove expense from its label
+  req.label.expenses = req.label.expenses.filter((expense) => expense !== expenseId);
+  await req.label.save();
+
+  // delete expense from expenses collection
+  const expense = await Expenses.findByIdAndDelete(expenseId);
+
+  res.status(200).json({
+    message: "expense deleted successfully",
+    deletedExpense: expense,
+  });
+  } catch (error) {
+    error.message = "Invalid Expense Id";
+    error.status = 400;
+    return next(error);
+  }
+};
--- backend/routes/expenses.js
+++ backend/routes/expenses.js
@@ -1,13 +1,21 @@
 const express = require("express");
 
 const isAuth = require("../middleware/is_auth");
+const hasLabel = require("../middleware/has_label");
 const expensesController = require("../Controllers/Expenses");
 
 const router = express.Router();
 
-router.get("/expenses", isAuth, expensesController.getExpenses);
-router.put("/expense", isAuth, expensesController.addExpense);
-router.delete("/expense", isAuth, expensesController.deleteExpense);
+router.get("/expenses/:labelId", isAuth, expensesController.getExpenses);
+router.put("/expense", isAuth, hasLabel, expensesController.addExpense);
+router.delete("/expense", isAuth, hasLabel, expensesController.deleteExpense);
+
+const labelController = require("../Controllers/Labels");
+
+router.get("/labels", isAuth, labelController.getLabels);
+router.put("/label", isAuth, labelController.addLabel);
+router.patch("/label", isAuth, hasLabel, labelController.editLabel);
+router.delete("/label", isAuth, hasLabel, labelController.deleteLabel);
 
 module.exports = router;
 
