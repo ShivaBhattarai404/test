4f6107dd05c2c693ae92f05c40169f45b5ad91f4
Shiva <shivabhattarai15@gmail.com>
2024-01-12 11:04:08 +0545

    Add logout feature

--- frontend/src/Components/MainNavigation/MainNavigation.jsx
+++ frontend/src/Components/MainNavigation/MainNavigation.jsx
@@ -1,22 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+
 import classes from "./MainNavigation.module.css";
-import { NavLink } from "react-router-dom";
+import Button from "../UI/Button/Button";
+import Modal from "../UI/Modal/Modal";
+
 import { IoAddCircleOutline, IoHomeSharp } from "react-icons/io5";
 import { IoMdCalculator } from "react-icons/io";
 
 const MainNavigation = () => {
+  const [modal, setModal] = useState(null);
+  const navigate = useNavigate();
+
+  const logoutHandler = () =>{
+    localStorage.removeItem("token");
+    localStorage.removeItem("expiryDate");
+    navigate("/login");
+  }
+  const logoutBtnClickHandler = (e) =>{
+    setModal(true)
+  }
   return (
     <nav className={classes.nav}>
+      {modal && (
+        <Modal title="Confirmation" confirmText="Logout" onConfirm={logoutHandler} onCancel={setModal.bind(null, null)} >
+          <h1>Do you want to logout?</h1>
+        </Modal>
+      )}
       <header className={classes.nav__header}>
-      <span><IoMdCalculator /></span>
-        <h1>Expense Tracker App</h1>
+        <h1>
+          <IoMdCalculator />
+          <span>Expense Tracker App</span>
+        </h1>
+        <div className={classes.userBox}>
+          <div className={classes.userBox__username}>Welcome! Shiva</div>
+          <Button className={classes.logoutBtn} onClick={logoutBtnClickHandler}>Logout</Button>
+        </div>
       </header>
       <ul className={classes.nav__ul}>
         <li>
-          <NavLink to="/"><IoHomeSharp /><span>Home</span></NavLink>
+          <NavLink to="/">
+            <IoHomeSharp />
+            <span>Home</span>
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/new"><IoAddCircleOutline /><span>Add a Label</span></NavLink>
+          <NavLink to="/new">
+            <IoAddCircleOutline />
+            <span>Add a Label</span>
+          </NavLink>
         </li>
       </ul>
     </nav>
